396850de37ed41954ed995085b191f44ffc7d692
Mya Von Behren <mvonbehr@depaul.edu>
2025-04-14 01:08:19 -0500

    Connect storyboard UI elements

--- mvonbehr-hw1/mvonbehr-hw1/ViewController.swift
+++ mvonbehr-hw1/mvonbehr-hw1/ViewController.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var chartView: ChartView!
+    @IBOutlet weak var sortButton: UIButton!
+    @IBOutlet weak var sizeSegControl: UISegmentedControl!
+    @IBOutlet weak var algorithmPickerTop: UISegmentedControl!
+    @IBOutlet weak var algorithmPickerBottom: UISegmentedControl!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
