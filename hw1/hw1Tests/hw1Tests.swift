99c2c5b7405b071b558a102e185d8b84decc25ce
Mya Von Behren <mvonbehr@depaul.edu>
2025-04-13 23:46:39 -0500

    Create Xcode project

---
+++ hw1/hw1Tests/hw1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  hw1Tests.swift
+//  hw1Tests
+//
+//  Created by Mya Von Behren on 4/13/25.
+//
+
+import Testing
+@testable import hw1
+
+struct hw1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ hw1/hw1UITests/hw1UITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  hw1UITestsLaunchTests.swift
+//  hw1UITests
+//
+//  Created by Mya Von Behren on 4/13/25.
+//
+
+import XCTest
+
+final class hw1UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
