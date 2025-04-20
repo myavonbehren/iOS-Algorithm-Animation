5d89e861fe3b2be6fb9fe879a842c804a1d7aac0
Mya Von Behren <mvonbehr@depaul.edu>
2025-04-20 00:12:06 -0500

    Creating sorting algorithm class

---
+++ mvonbehr-hw1/mvonbehr-hw1/SortingAlgorithms.swift
@@ -0,0 +1,36 @@
+//
+//  SortingAlgorithms.swift
+//  mvonbehr-hw1
+//
+//  Created by Mya Von Behren on 4/20/25.
+//
+
+import Foundation
+
+class SortingAlgorithms {
+    
+    func insertionSort(_ array: [Int]) -> [Int] {
+        return [0]
+    }
+            
+        
+        
+    func selectionSort(_ array: [Int]) -> [Int] {
+        return [0]
+        
+    }
+    
+    func quickSort(_ array: [Int]) -> [Int] {
+        return [0]
+        
+    }
+    
+    func mergeSort(_ array: [Int]) -> [Int] {
+        return [0]
+    }
+        
+        
+   
+    
+    
+}
